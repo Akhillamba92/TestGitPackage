e474c226938f400b2ba8772df6aa157660b05181
Akhillamba92 <akhil.lamba@rationalheads.com>
2015-10-29 15:30:49 +0530

    branch 2 ... 2nd change commit

--- TestGit/src/com/rh/Package2/Class2.java
+++ TestGit/src/com/rh/Package2/Class2.java
@@ -3,6 +3,8 @@ package com.rh.Package2;
 public class Class2 {
 	private void test2(){
 		System.out.println("hi");
+		System.out.println("1st change");
+		System.out.println("2nd change");
 	}
 
 }
