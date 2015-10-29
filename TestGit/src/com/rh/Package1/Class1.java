8b8d73ac115c9d8eecce7b60b84cf2b7b2d4398a
Akhillamba92 <akhil.lamba@rationalheads.com>
2015-10-29 14:24:39 +0530

    master commit
    test for git...master commit

---
+++ TestGit/src/com/rh/Package1/Class1.java
@@ -0,0 +1,7 @@
+package com.rh.Package1;
+
+public class Class1 {
+	private void test1(){
+		System.out.println("hi");
+	}
+}
---
+++ TestGit/src/com/rh/Package2/Class2.java
@@ -0,0 +1,8 @@
+package com.rh.Package2;
+
+public class Class2 {
+	private void test2(){
+		System.out.println("hi");
+	}
+
+}
