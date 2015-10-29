c26c1a11ff1afe373ca2aaf9467171f17072a99b
Akhillamba92 <akhil.lamba@rationalheads.com>
2015-10-29 15:18:46 +0530

    commit in m_branch 1
    branch 1 change

--- TestGit/src/com/rh/Package1/Class1.java
+++ TestGit/src/com/rh/Package1/Class1.java
@@ -3,5 +3,6 @@ package com.rh.Package1;
 public class Class1 {
 	private void test1(){
 		System.out.println("hi");
+		System.out.println("1st change");
 	}
 }
--- TestGit/src/com/rh/Package2/Class2.java
+++ TestGit/src/com/rh/Package2/Class2.java
@@ -3,6 +3,7 @@ package com.rh.Package2;
 public class Class2 {
 	private void test2(){
 		System.out.println("hi");
+		System.out.println("1st change");
 	}
 
 }
